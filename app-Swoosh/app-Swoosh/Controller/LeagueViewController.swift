28b0f82a860c7ccdfe679f5d7bd284cca9cd1ccb
Ario Nugroho <gamerzdope@gmail.com>
2019-08-14 19:46:39 +0700

    added couple view controller

--- app-Swoosh/app-Swoosh/Controller/LeagueViewController.swift
+++ app-Swoosh/app-Swoosh/Controller/LeagueViewController.swift
@@ -10,16 +10,58 @@ import UIKit
 
 class LeagueViewController: UIViewController {
 
+    var player : Player!
+    
+    @IBOutlet weak var nextButton: BorderButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        nextButton.isHidden = true
 
-        // Do any additional setup after loading the view.
+        player = Player()
+        
     }
+    
+    func inputPlayer(selectedPlayer : String) {
+        
+        player.desiredLeague = selectedPlayer
+        nextButton.isHidden = false
+        
+    }
+    
+    //MARK: - Every Button Options Action when pressed
+    //Never create logic inside button (IBaction) so create separate func then init it inside IBAction
+    
+    @IBAction func onMenPressed(_ sender: Any) {
+        
+        inputPlayer(selectedPlayer: "men")
+        
+    }
+    
+    @IBAction func onWomenPressed(_ sender: Any) {
+        
+        inputPlayer(selectedPlayer: "women")
+        
+    }
+    
+    @IBAction func onCoopPressed(_ sender: Any) {
+        
+        inputPlayer(selectedPlayer: "coop")
+        
+    }
+    
+    
+    
+    //MARK: - Programmatic button pressed
     
     @IBAction func onNextPressed(_ sender: Any) {
         
         performSegue(withIdentifier: "skillSegue", sender: self)
+        
     }
     
 
 }
+
+
---
+++ app-Swoosh/app-Swoosh/Model/Player.swift
@@ -0,0 +1,17 @@
+//
+//  Player.swift
+//  app-Swoosh
+//
+//  Created by Ario Nugroho on 14/08/19.
+//  Copyright © 2019 Ario Nugroho. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    
+    var desiredLeague : String?
+    var selectedSkillLevel : String?
+    
+    
+}
